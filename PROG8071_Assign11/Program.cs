2484d3e25397b55c3fa6d2e1737f1231faf5165d
Eldhose Raju <eldhosektm@gmail.com>
2023-02-08 11:19:36 -0500

    Containing the completed Console Application

--- PROG8071_Assign11/Program.cs
+++ PROG8071_Assign11/Program.cs
@@ -10,7 +10,59 @@ namespace Assignment2
     {
         static void Main(string[] args)
         {
-         Circle circle= new Circle();
+
+            int option;
+            Console.WriteLine("enter the radius");
+            //circle.SetRadius(Convert.ToInt32(Console.ReadLine()));
+            int radius;
+            while (!int.TryParse(Console.ReadLine(), out radius) || radius <= 0)
+            {
+                Console.WriteLine("Invalid input. Please enter an integer greater than zero:");
+            }
+            Circle circle = new Circle(radius);
+            do
+            {
+
+
+                Console.WriteLine("enter your option");
+
+                Console.WriteLine("1.get circle Radius");
+
+                Console.WriteLine("2.Change Circle Radius");
+
+                Console.WriteLine("3.get circle circumference");
+
+                Console.WriteLine("4.get circle area");
+
+                Console.WriteLine("5.Exit");
+
+
+
+
+
+                option = Convert.ToInt32(Console.ReadLine());
+
+                if (option == 1)
+                {
+                    Console.WriteLine("The radius is" + circle.GetRadius());
+
+                }
+                if (option == 2)
+                {
+                    Console.WriteLine("Please enter the radius you want to set");
+                    circle.SetRadius(Convert.ToInt32(Console.ReadLine()));
+                }
+                if (option == 3)
+                {
+                    Console.WriteLine("the Circumference is" + circle.GetCircumference());
+                }
+                if (option == 4)
+                {
+                    Console.WriteLine("the area is" + circle.GetArea());
+                }
+
+
+            } while (option != 5);
         }
     }
 }
